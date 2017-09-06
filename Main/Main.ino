4446c7847c8550dd6c4e6373dbd0a17c4cde9228
fahim chowdhury <fahim@gmx.com>
2017-09-06 16:37:36 +0100

    added a test url call

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,18 +1,66 @@
-#include "src/WIFISetup.h";
-#include "src/SetupServer.h";
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
 
-WIFISetup wifiSetup = WIFISetup();
-SetupServer* setupServer;
+//needed for library
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include "WiFiManager.h"          //https://github.com/tzapu/WiFiManager
+#include <ESP8266HTTPClient.h>
+
+void configModeCallback (WiFiManager *myWiFiManager) {
+  Serial.println("Entered config mode");
+  Serial.println(WiFi.softAPIP());
+  //if you used auto generated SSID, print it
+  Serial.println(myWiFiManager->getConfigPortalSSID());
+}
 
 void setup() {
-  wifiSetup.init();
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wifiManager;
+  //reset settings - for testing
+  //wifiManager.resetSettings();
+
+  //set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
+  wifiManager.setAPCallback(configModeCallback);
+
+  //fetches ssid and pass and tries to connect
+  //if it does not connect it starts an access point with the specified name
+  //here  "AutoConnectAP"
+  //and goes into a blocking loop awaiting configuration
+  if(!wifiManager.autoConnect()) {
+    Serial.println("failed to connect and hit timeout");
+    //reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(1000);
+  } 
+
+  //if you get here you have connected to the WiFi
+  Serial.println("connected...yeey :)");
+
+  HTTPClient http;
+  Serial.print("[HTTP] begin...\n");
+  http.begin("http://google.com");
+  Serial.print("[HTTP] GET...\n");
+  int httpCode = http.GET();
+  if(httpCode > 0) {
+     // HTTP header has been send and Server response header has been handled
+     Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+     // file found at server
+     if(httpCode == HTTP_CODE_OK) {
+        String payload = http.getString();
+        Serial.println(payload);
+            }
+     } else {
+       Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+   }
+  http.end();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  wifiSetup.scan();
-  setupServer = new SetupServer();
-  setupServer->updateSSIDList(wifiSetup.getSSIDList());
-  setupServer->start();
+
 }
