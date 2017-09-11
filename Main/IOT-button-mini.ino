5fdacddf7b1ea8e721c92ed9d1380da2d56268ef
Fahim Chowdhury <fahim.chowdhury1985@gmail.com>
2017-09-11 23:51:46 +0100

    added mini

--- Main/IOT-button-mini.ino
+++ Main/IOT-button-mini.ino
@@ -5,9 +5,15 @@
 #include <ESP8266WebServer.h>
 #include "WiFiManager.h"          //https://github.com/tzapu/WiFiManager
 #include <ESP8266HTTPClient.h>
-int buttonState = 0; 
 int readyState = 0;
-WiFiManagerParameter wifiParameter("actionURL","set action url","",200);
+WiFiManagerParameter wifiParameter("actionURL","Set Action URL","",200);
+
+void configModeCallback (WiFiManager *myWiFiManager) {
+  Serial.println("Entered config mode");
+  Serial.println(WiFi.softAPIP());
+  //if you used auto generated SSID, print it
+  Serial.println(myWiFiManager->getConfigPortalSSID());
+}
 
 void saveURL(String url){
   int charLength=url.length();
@@ -15,7 +21,7 @@ void saveURL(String url){
   Serial.println("writing eeprom ssid:");
           for (int i = 0; i < 200; ++i)
             {
-              EEPROM.write(i, url[i]);
+              EEPROM.write(i,  url[i]);
               Serial.print("Wrote: ");
               Serial.println(url[i]); 
             }
@@ -27,7 +33,11 @@ String readUrl(){
   for (int i = 0; i < 200; ++i)
     {
       char _byte = char(EEPROM.read(i));
-      url += _byte;  
+      if(_byte) {
+        url += _byte; 
+        Serial.println(_byte);  
+      }
+      
     }
     //esid.trim();
     Serial.println(url.length());
@@ -37,9 +47,14 @@ String readUrl(){
   return url;
 }
 
+void saveConfigCallback(){
+  Serial.println("get param value");
+  Serial.println(wifiParameter.getValue());
+  saveURL(wifiParameter.getValue());
+}
+
 void makeRequest(){
-  //String url = readUrl();
-  String url = "http://maker.ifttt.com/trigger/button_pressed/with/key/dnZFBpYIBLexQjFcj2zXb33bEQnB5xQaiRrrSEa1-_s";
+  String url = readUrl();
   HTTPClient http;
   Serial.print("[HTTP] begin...\n");
   http.begin(url);
@@ -59,41 +74,28 @@ void makeRequest(){
        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
    }
   http.end();
-  digitalWrite(D2, HIGH);
-}
-
-void saveConfigCallback(){
-  Serial.println("get param value");
-  Serial.println(wifiParameter.getValue());
-  //saveURL(wifiParameter.getValue());
+  digitalWrite(4, HIGH);
 }
 
 
-void configModeCallback (WiFiManager *myWiFiManager) {
-  Serial.println("Entered config mode");
-  Serial.println(WiFi.softAPIP());
-  //if you used auto generated SSID, print it
-  Serial.println(myWiFiManager->getConfigPortalSSID());
-}
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  //EEPROM.begin(512);
-  pinMode(D2, OUTPUT);
-  pinMode(D1, INPUT);
+   Serial.println("setup");
+  EEPROM.begin(512);
+   Serial.println("EEPROM");
+  pinMode(4, OUTPUT);
   //WiFiManager
   //Local intialization. Once its business is done, there is no need to keep it around
   WiFiManager wifiManager;
-  
- 
   //reset settings - for testing
   //wifiManager.resetSettings();
   wifiManager.addParameter(&wifiParameter);
   //set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
   wifiManager.setAPCallback(configModeCallback);
   wifiManager.setSaveConfigCallback(saveConfigCallback);
-
+  
   //fetches ssid and pass and tries to connect
   //if it does not connect it starts an access point with the specified name
   //here  "AutoConnectAP"
@@ -104,23 +106,21 @@ void setup() {
     ESP.reset();
     delay(1000);
   } 
-  
+
   //if you get here you have connected to the WiFi
   Serial.println("connected...yeey :)");
   readyState = 1;
-  digitalWrite(D2, HIGH);
+  digitalWrite(4, HIGH);
 }
-
-
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int previousButtonState = buttonState;
-  buttonState = digitalRead(D1);
-  if(readyState && previousButtonState != buttonState)
+  if(readyState)
   {
-    digitalWrite(D2, LOW);
+    readyState=0;
+    digitalWrite(4, LOW);
     makeRequest();
+    ESP.deepSleep(0);
   }
-
+  
 }
+
